917a689af34b6b40b7e96dca4f9a47271ad3be22
Aaron Weiss <aaronweiss74@gmail.com>
2013-08-23 05:12:15 -0400

    Added necessary gems and updated app.rb with routes.

--- app.rb
+++ app.rb
@@ -1,6 +1,30 @@
+require "yaml"
 require "bundler"
 Bundler.require :default, :environment
 
-get '/hi' do
-  "Hello World!"
+before do
+	# Before you go, you may want to configure these.
+	@author = "Aaron Weiss"
+	# @home = "main site address"
+	# @analytics_id = "Google Analytics campaign id"
+	# @analytics_dm = "Google Analytics domain"
+end
+
+get '/' do
+	yaml = YAML.load_file "books.yml"
+	@books = yaml["books"]
+	erb :index
+end
+
+get '/clear' do
+	cache_clear
+	redirect to('/')
+end
+
+get "/css/bibliome.css" do
+	scss :bibliome
+end
+
+get "/back" do
+	redirect to(@home)
 end
